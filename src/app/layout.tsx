9453979bc7c1979616ec310d48581ce5f17ee420
Tran Quang <tranquang@example.com>
2025-04-03 19:30:46 +0700

    Fix layout.tsx to work on server side

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { getServerSession } from 'next-auth'
@@ -9,6 +7,7 @@ import Navbar from '@/components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './globals.css'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,7 +26,7 @@ export default async function RootLayout({
   return (
     <html lang="vi">
       <head>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" />
       </head>
       <body className={inter.className}>
         <Navbar session={session} />
